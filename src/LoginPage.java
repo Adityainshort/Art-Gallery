c6159718d741501b974a5cbc9c29b877478b1771
Aman Kumar Yadav <148520638+aman-5137@users.noreply.github.com>
2024-01-23 10:19:24 +0530

    gedg
    hjrthjrt

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -18,6 +18,7 @@ public class LoginPage {
 
         existingUser = new JButton("Existing User");
         existingUser.setFocusable(false);
+        existingUser.addActionListener(e -> new ExistingUser());
         existingUser.setBounds(180,200,220,30);
         existingUser.setBackground(Color.DARK_GRAY);
         existingUser.setForeground(Color.WHITE);
@@ -54,23 +55,162 @@ class NewUser{
 
     JFrame frame;
     JTextField f1,f2;
+    JLabel l1,l2;
+    JButton submitButton, backButton;
+
     NewUser(){
 
         f1 = new JTextField();
         f1.setBounds(180,100,220,30);
+        l1=new JLabel("Enter Name");
+        l1.setForeground(Color.DARK_GRAY);
+        l1.setBounds(180,75,220,30);
 
 
         f2 = new JTextField();
+        f2.setBounds(180,200,220,30);
+        l2 = new JLabel("Enter Password");
+        l2.setBounds(180,175,220,30);
+        l2.setForeground(Color.DARK_GRAY);
+
+        submitButton= new JButton("Submit");
+        submitButton.setFocusable(false);
+        submitButton.setBounds(180,250,220,30);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                Connect JDBC code here
+            }
+        });
+        submitButton.addActionListener(e -> new NewUser());
+        submitButton.setBackground(Color.DARK_GRAY);
+        submitButton.setForeground(Color.WHITE);
+
+
+        backButton = new JButton("Back");
+        backButton.setFocusable(false);
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new LoginPage();
+            }
+        });
+        backButton.setBounds(10,8,70,30);
+        backButton.setBackground(Color.DARK_GRAY);
+        backButton.setForeground(Color.WHITE);
+
 
         frame = new JFrame("Login");
         frame.setLayout(null);
         frame.setVisible(true);
-        frame.getContentPane().setBackground(new Color(0xDA6400BD, true));
+        frame.getContentPane().setBackground(Color.WHITE);
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(f1);
+        frame.add(l1);
+        frame.add(f2);
+        frame.add(l2);
+        frame.add(submitButton);
+        frame.add(backButton);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
 
     }
 }
+   class ExistingUser{
+        JFrame frame2;
+
+        JTextField f1,f2;
+
+        JLabel l1,l2;
+
+        JButton submitButton, backButton;
+
+        ExistingUser(){
+
+            f1 = new JTextField();
+            f1.setBounds(180,100,220,30);
+            l1=new JLabel("Enter Name");
+            l1.setForeground(Color.DARK_GRAY);
+            l1.setBounds(180,75,220,30);
+
+
+            f2 = new JTextField();
+            f2.setBounds(180,200,220,30);
+            l2 = new JLabel("Enter Password");
+            l2.setBounds(180,175,220,30);
+            l2.setForeground(Color.DARK_GRAY);
+
+            submitButton= new JButton("Submit");
+            submitButton.setFocusable(false);
+            submitButton.setBounds(180,250,220,30);
+            submitButton.addActionListener(e -> new NewUser());
+
+            submitButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+//                    add JDBC data here
+
+                    try{
+
+                        int n=0;
+                        while(rs.next()){
+                            String name= rs.getString("UserName");
+                            int id = sr.getInt("UderId");
+
+
+                            if(f1.getText().equals(name)  && f2.getText().equals(id)){
+                                n=1;
+                                break;
+                            }else{
+                                n=0;
+                            }
+                        }
+
+                        if(n==1){
+                            frame2.dispose();
+                        }else{
+                            
+                        }
+
+                    }catch (Exception a){
+                        System.out.println("Error in finding.");
+                    }
+                }
+            });
+            submitButton.setBackground(Color.DARK_GRAY);
+            submitButton.setForeground(Color.WHITE);
+
+
+            backButton = new JButton("Back");
+            backButton.setFocusable(false);
+            backButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    frame2.dispose();
+                    new LoginPage();
+                }
+            });
+            backButton.setBounds(10,8,70,30);
+            backButton.setBackground(Color.DARK_GRAY);
+            backButton.setForeground(Color.WHITE);
+
+
+            frame2 = new JFrame("Login");
+            frame2.setLayout(null);
+            frame2.setVisible(true);
+            frame2.getContentPane().setBackground(Color.WHITE);
+            frame2.setSize(600, 400);
+            frame2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame2.add(f1);
+            frame2.add(l1);
+            frame2.add(f2);
+            frame2.add(l2);
+            frame2.add(submitButton);
+            frame2.add(backButton);
+            frame2.setLocationRelativeTo(null);
+            frame2.setResizable(false);
+
+
+}}
