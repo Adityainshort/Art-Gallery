1462ed10cb1feaa0943ce393fb65cb108a1ba4da
Aditya Raj Singh <120239595+Adityainshort@users.noreply.github.com>
2024-01-23 12:29:09 +0530

    added existing user class in loginpage

--- LoginPage.java
+++ LoginPage.java
@@ -127,3 +127,82 @@ class NewUser{
 
     }
 }
+
+class ExistingUser{
+        JFrame frame2;
+
+        JTextField f1,f2;
+
+        JLabel l1,l2;
+
+        JButton submitButton, backButton;
+
+        ExistingUser(){
+
+            f1 = new JTextField();
+            f1.setBounds(180,100,220,30);
+            l1=new JLabel("Enter Name");
+            l1.setForeground(Color.DARK_GRAY);
+            l1.setBounds(180,75,220,30);
+
+
+            f2 = new JTextField();
+            f2.setBounds(180,200,220,30);
+            l2 = new JLabel("Enter Password");
+            l2.setBounds(180,175,220,30);
+            l2.setForeground(Color.DARK_GRAY);
+
+            submitButton= new JButton("Submit");
+            submitButton.setFocusable(false);
+            submitButton.setBounds(180,250,220,30);
+//            submitButton.addActionListener(e -> new NewUser());
+
+            submitButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(f1.getText().isEmpty()||f2.getText().isEmpty()){
+                        frame2.dispose();
+                        
+                    }
+
+
+
+
+                    frame2.dispose();
+                }
+            });
+            submitButton.setBackground(Color.DARK_GRAY);
+            submitButton.setForeground(Color.WHITE);
+
+
+            backButton = new JButton("Back");
+            backButton.setFocusable(false);
+            backButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    frame2.dispose();
+                    new LoginPage();
+                }
+            });
+            backButton.setBounds(10,8,70,30);
+            backButton.setBackground(Color.DARK_GRAY);
+            backButton.setForeground(Color.WHITE);
+
+
+            frame2 = new JFrame("Login");
+            frame2.setLayout(null);
+            frame2.setVisible(true);
+            frame2.getContentPane().setBackground(Color.WHITE);
+            frame2.setSize(600, 400);
+            frame2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame2.add(f1);
+            frame2.add(l1);
+            frame2.add(f2);
+            frame2.add(l2);
+            frame2.add(submitButton);
+            frame2.add(backButton);
+            frame2.setLocationRelativeTo(null);
+            frame2.setResizable(false);
+
+}}
